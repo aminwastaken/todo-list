b949ee905810f5132c50489108508e5bf2b6e331
aminwastaken <aminwastaken@gmail.com>
2021-01-22 11:31:53 +0100

    Link app to server and database

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const app = express();
+const db = require('./persistence');
+const getItems = require('./routes/getItems');
+const addItem = require('./routes/addItem');
+const updateItem = require('./routes/updateItem');
+const deleteItem = require('./routes/deleteItem');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+app.use(require('body-parser').json());
+app.use(express.static(__dirname + '/static'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/items', getItems);
+app.post('/items', addItem);
+app.put('/items/:id', updateItem);
+app.delete('/items/:id', deleteItem);
+
+db.init().then(() => {
+    app.listen(3000, () => console.log('Listening on port 3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
--- src/utils/add.js
+++
@@ -1,4 +0,0 @@
-export function addItem(list, taskName) {
-  if (!taskName) return;
-  return list.push({ id: list.length() + 1, value: taskName });
-}
